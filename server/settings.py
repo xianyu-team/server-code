6b9bf978ecc78022e554ec72f592613181ff89d7
watchcat2k <810549046@qq.com>
2019-04-23 16:51:36 +0800

    finish api: POST /user

--- server/settings.py
+++ server/settings.py
@@ -118,6 +118,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# url的尾部不要加斜杠，避免url匹配错误
+APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- user/views.py
+++ user/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
+import json
 
 from . import models
 
 # Create your views here.
 
+__ok__ = {
+    'code': 200,
+    'message': 'OK'
+}
+__error__ = {
+    'code': 400,
+    'message': 'ERROR'
+}
+
+# 增加装饰器，跳过csrf的保护，前端请求就不会被forbidden
+# 或者在前端做csrf保护请求方式
+@csrf_exempt
 def user(request):
-    user = models.User.objects.get(pk=1)
-    return render(request, 'user/test.html', {'user': user})
+    if request.method == 'POST':
+        parameters = request.POST
+        new_user = models.User(
+            user_phone=parameters['user_phone'],
+            user_password=parameters['user_password']
+        )
+        new_user.save()
+        return HttpResponse(json.dumps(__ok__), content_type='application/json')
+    elif request.method == 'GET':
+        user = models.User.objects.get(id=1)
+        return render(request, 'user/test.html', {'user': user})
+    
